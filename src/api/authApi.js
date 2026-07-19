4576897b13547ebc1afcd0ab5ecb4b5a98496e99
DuaIbrahim <ibrahimdua0123@gmail.com>
2026-07-19 18:56:26 +0500

    Fix auth API URL

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: "https://post-app-backend-lilac.vercel.app/api/auth",
 });
 
 export default API;
